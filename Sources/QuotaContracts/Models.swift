e1327c83e2a108f9a74e47068768206dc860c0b9
joetam <1564331+joetam@users.noreply.github.com>
2026-08-06 17:20:06 -0700

    Version the provider icon contract upgrade

--- Sources/QuotaContracts/Models.swift
+++ Sources/QuotaContracts/Models.swift
@@ -2,8 +2,8 @@ import Foundation
 
 public let quotaContractVersion = 1
 /// Bump when clients must replace an already-running app-server after an update.
-public let quotaAppServerAPIVersion = 3
-public let quotaReleaseVersion = "0.1.0"
+public let quotaAppServerAPIVersion = 4
+public let quotaReleaseVersion = "0.1.1"
 
 public enum AuthenticationState: String, Codable, Sendable {
     case authenticated
